7c2d62f78c19ab75690e97ec4f4022edd3476364
gotosh <goto.shunsuke.53st@gmail.com>
2025-01-24 15:06:50 +0900

    Working on modifying boundary condition. The goal is scanning another txt file and boundary class read this file. Another task is handling with changes of CAD data.

--- include/boundary.hpp
+++ include/boundary.hpp
@@ -4,18 +4,36 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
-
+#include <istream>
+#include <string>
+#include <tuple>
+#include <algorithm>
+#include <cctype>
 
 class boundary
 {
 private:
     int count;
+
 public:
+    const double NEG_INF = -1e9;
+    const double POS_INF = 1e9;
     boundary(int N_x, int N_y);
     void initialize(std::vector<std::vector<double>> &phi);
     void boundary_condition(std::vector<std::vector<double>> &phi, double dL_x, double dL_y);
     void boundary_condition_2(std::vector<std::vector<double>> &phi, double dL_x, double dL_y);
+    bool isWhitespace(const std::string& str);
+    bool parseExpression(const std::string& expr, double& minValue, double& maxValue);
+    
     std::vector<std::vector<int>> flags(std::vector<std::vector<double>> &boundary);
+
+    struct Condition
+    {
+        double y_min, y_max;
+        double x_min, x_max;
+        double boundary;
+    };
+    
 
 
     ~boundary();
--- src/boundary.cpp
+++ src/boundary.cpp
@@ -73,6 +73,59 @@ void boundary::boundary_condition_2(std::vector<std::vector<double>> &phi, doubl
     
 }
 
+/* Confirm whether string is consists of white spaces */
+bool isWhitespace(const std::string& str) {
+    return std::all_of(str.begin(), str.end(), [](unsigned char c) {
+        return std::isspace(c);
+    });
+}
+
+/* Parse inequality or quality */
+bool boundary::parseExpression(const std::string& expr, double& minValue, double& maxValue) {
+    // initial value //
+    minValue = NEG_INF;
+    maxValue = POS_INF;
+
+    /* One sided inequality (ex. a < y) */ 
+    if (expr.find('<') != std::string::npos && expr.find('>') == std::string::npos)
+    {
+        double value;
+        if (sscanf(expr.c_str(), " %lf < %*s", &value) == 1)
+        {
+            minValue = value;
+            return true;
+        }
+        if (sscanf(expr.c_str(), " %*s < %lf", &value) == 1)
+        {
+            maxValue = value;
+            return true;
+        }
+        
+    }
+    /* Bi-sided equation */
+    if (expr.find('<') != std::string::npos)
+    {
+        if (sscanf(expr.c_str(), " %lf < %*s < %lf", &minValue, &maxValue) == 2)
+        {
+            return true;
+        }
+                
+    }
+    
+    /* Equation (ex. "y = 80.0") */
+    if (expr.find('=') != std::string::npos)
+    {
+        double value;
+        if (scanf(expr.c_str(), " %*s = %lf", &value) == 1)
+        {
+            minValue = maxValue = value;
+            return true;
+        }
+                
+    }
+    return false;
+
+}
 std::vector<std::vector<int>> boundary::flags(std::vector<std::vector<double>> &boundary){
     std::vector<std::vector<int>> flag(boundary.size(), std::vector<int>(boundary[0].size(), 0));
     
